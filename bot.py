794d32aaeea0e2049cf4b91b2dd9995e8cc36029
sherloco <sherloco@posteo.de>
2018-11-15 01:04:37 +0100

    added telegram commands to pull from git, update environment and restart bot added command line parameter for dev mode

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, Filters
+import os
+import sys
+from threading import Thread
 
 devMode = False
 
 token = None
 updater = None
+admin = None
 
 
 def start(bot, update):
@@ -13,6 +17,7 @@ def start(bot, update):
         'dem Speiseplan schicke. Dein Abo kannst mit /show_sub überprüfen oder mit /unsub_daily_menu jederzeit '
         'widerrufen. Die ausgegebenen Daten der Speisekarte stammen von der Website der Kantine und unterliegen dem '
         'entsprechenden Urheberrecht.')
+
 
 def menu(bot, update, args):
     """Send the menu."""
@@ -111,11 +116,63 @@ def unset_daily_menu(bot, update, chat_data):
     update.message.reply_text('Dein Abo wurde erfolgreich entfernt.')
 
 
+def shut_down(bot, update):
+    update.message.reply_text('Shutting down...')
+    os._exit(0)
+
+
+def stop_and_restart():
+    """Gracefully stop the Updater and replace the current process with a new one"""
+    updater.stop()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+def restart(bot, update):
+    update.message.reply_text('Bot is restarting...')
+    Thread(target=stop_and_restart).start()
+
+
+def pull_update_env_and_restart(bot, update):
+    pull(bot, update)
+    update_env(bot, update)
+    restart(bot, update)
+
+
+def pull(bot, update):
+    update.message.reply_text('Bot is pulling...')
+    try:
+        import git
+        g = git.cmd.Git('.')
+        out = g.pull('https://github.com/sherloco/schmankerl-bot.git')
+        print(out)
+        update.message.reply_text('Bot has finished pulling with message:\n' + out)
+    except:
+        import traceback
+        traceback.print_exc()
+        update.message.reply_text('Pull failed:\n' + traceback.format_exc())
+
+
+def update_env(bot, update):
+    try:
+        update.message.reply_text('Starting update...')
+        import subprocess
+        subprocess.call('conda env update -f=schmankerl-bot-env.yml')
+        update.message.reply_text('Update successful.')
+    except:
+        import traceback
+        traceback.print_exc()
+        update.message.reply_text('Update failed:\n' + traceback.format_exc())
+
+
 def main():
     """Run bot."""
+    parse_command_line_arguments()
 
     if token is None:
         load_token_from_file()
+
+    if admin is None:
+        load_admin_from_file()
 
     global updater
     updater = Updater(token)
@@ -142,6 +199,12 @@ def main():
                                   show_daily_menu,
                                   pass_chat_data=True))
 
+    dp.add_handler(CommandHandler('restart', restart, filters=Filters.user(username=admin)))
+    dp.add_handler(CommandHandler('update_env', update_env, filters=Filters.user(username=admin)))
+    dp.add_handler(CommandHandler('pull', pull, filters=Filters.user(username=admin)))
+    dp.add_handler(CommandHandler('pur', pull_update_env_and_restart, filters=Filters.user(username=admin)))
+    dp.add_handler(CommandHandler('shut_down', shut_down, filters=Filters.user(username=admin)))
+
     # log all errors
     dp.add_error_handler(error)
 
@@ -163,6 +226,14 @@ def load_token_from_file():
     global token
     token = token_file.read()
     token_file.close()
+
+
+def load_admin_from_file():
+    import codecs
+    admin_file = codecs.open('./admin.dat', encoding='utf-8')
+    global admin
+    admin = admin_file.read()
+    admin_file.close()
 
 
 def decode_weekday(weekday: str):
@@ -202,6 +273,26 @@ def decode_weekday(weekday: str):
         'sunday': 6
     }
     return switcher.get(weekday, "Invalid day")
+
+
+def parse_command_line_arguments():
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--devMode", type=str2bool, nargs='?', default=False, dest='devMode',
+                        const=True, help="Runs the bot under the development token.")
+    args = parser.parse_args()
+    global devMode
+    devMode = args.devMode
+
+
+def str2bool(v):
+    import argparse
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
 if __name__ == '__main__':
